ee14ff52ba5e8555600d882fcfcd7de2cd315ea6
Arnab Santra <arnabsantra365@gmail.com>
2024-06-17 10:09:29 +0530

    frontend and backend connected and proxy fixed

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const con = require('./connection')
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin: true }));
@@ -15,6 +15,26 @@ con.connect(err => {
         console.log(result);
     })
   });
+
+
+  app.post('/api/form', (req, res) => {
+    const {  name, countryCode, phoneNumber } = req.body;
+    
+    console.log(name);
+    // Perform validation checks here
+    
+    // Insert into MySQL database
+    // const sql = `INSERT INTO phonedata ( name, country_code, phone_number) VALUES ( ?, ?, ?)`;
+    // con.query(sql, [ name, countryCode, phoneNumber], (err, result) => {
+    //   if (err) {
+    //     console.error('Error inserting data into database:', err);
+    //     res.status(500).send('Error inserting data into database');
+    //   } else {
+    //     res.status(200).send('Form data inserted successfully');
+    //   }
+    // });
+  });
+
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
