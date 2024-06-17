d67db67978eb77b3a10279fa1580b5368907584d
Arnab Santra <arnabsantra365@gmail.com>
2024-06-17 20:08:24 +0530

    frontend url cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: "https://delightful-liger-3725d9.netlify.app/" , credentials:true }));
+app.use(cors({ origin: "https://delightful-liger-3725d9.netlify.app/" }));
 con.connect(err => {
     if (err) throw err;
     console.log('Connected to MySQL database');
